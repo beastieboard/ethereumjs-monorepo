d6e9c6defc34ac84efbbe5110244673823243902
ssadler <github@mashi.org>
2022-09-23 00:58:28 +0200

    init

---
+++ src/precompiles/types.ts
@@ -0,0 +1,14 @@
+import { ExecResult } from '../evm'
+import { EVMInterface } from '../types'
+import { Common } from '@ethereumjs/common'
+
+export interface PrecompileFunc {
+  (input: PrecompileInput): Promise<ExecResult> | ExecResult
+}
+
+export interface PrecompileInput {
+  data: Buffer
+  gasLimit: bigint
+  _common: Common
+  _EVM: EVMInterface
+}
